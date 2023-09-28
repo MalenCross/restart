ba45ce259db7f4cc0eb88f70330489cffc7ded51
Malen Cross <MalenCross@yahoo.com>
2023-09-27 21:27:57 -0500

    Tring to clean up the probem

--- Start Over/src/Sum.ts
+++ Start Over/src/Sum.ts
@@ -13,3 +13,46 @@ export function SumOfSquares(x:number, y:number) :number {
 export function Greeting(name:string) :string {
 	return "Hello " + name;
 }
+export function GetFirst(arr:number[]) :number {
+	return arr[0];
+}
+
+// export interface Card {
+// 	suit: string
+// 	face: number
+// }
+
+// let card1 = {suit: "hearts", face : 2};
+// card1.face
+// card1.suit
+
+export function GetFace(cardString:string) :any {
+	let firstLetter = cardString[0];
+	let face = 0;
+	if(firstLetter === 'A') {
+		face = 14;
+	}
+	if(firstLetter === 'K') {
+		face = 13;
+	}
+	if(firstLetter === 'Q') {
+		face = 12;
+	}
+	if(firstLetter === 'J') {
+		face = 11;
+	}
+	if(firstLetter === 'T') {
+		face = 10;
+	} else {
+		face = parseInt(firstLetter)
+	}
+	return face;
+}
+
+export function GetSuit(cardString:string) :string {
+	return cardString;
+}
+
+// export function GetCard(cardString:string) : any {
+// 	return undefined;
+// }
--- Start Over/src/__tests__/Sum.test.ts
+++ Start Over/src/__tests__/Sum.test.ts
@@ -1,4 +1,4 @@
-import {Sum, Div, SumOfSquares, Greeting} from "../Sum.ts";
+import {Sum, Div, SumOfSquares, Greeting, GetFirst, GetFace, GetSuit} from "../Sum.ts";
 test('Sum(1,2) = 3', () => {
   let result = Sum(1, 2);
   expect(result).toBe(3)
@@ -17,3 +17,30 @@ test('div(8,2) = 4', () => {
   let result =  Greeting("Malen");
   expect(result).toBe("Hello Malen")
 })
+test('get the first element from an array', () => {
+    let result = GetFirst([5,2,3,4]);
+    expect(result).toBe(5)
+  })
+  
+  // winner("2H 4D 5S 8C 9C", "4H 4S 5D 8S 9H") ->  "Player 2 Wins - Pair of fours"
+  
+  test('get the face value from a poker card', () => {
+    expect(GetFace("2H")).toBe(2);
+    expect(GetFace("3H")).toBe(3);
+    expect(GetFace("5H")).toBe(5);
+    expect(GetFace("TH")).toBe(10);
+    expect(GetFace("JS")).toBe(11)
+    expect(GetFace("QH")).toBe(12);
+    expect(GetFace("KH")).toBe(13);
+    expect(GetFace("AS")).toBe(14)
+  });
+  
+  test('get the suit from a poker card', () => {
+    expect(GetSuit("2H")).toBe('hearts');
+  })
+  
+  // test('get the card from a poker card string', () => {
+  //   let card = GetCard("2H")
+  //   expect(card.suit).toBe('hearts');
+  //   expect(card.face).toBe(2);
+  // })
