0d6594952db3236307d9f65fb777dcb4f68ad0bf
Malen Cross <MalenCross@yahoo.com>
2023-10-03 21:38:55 -0500

    save after lession before i wreck it

--- Start Over/src/Poker.ts
+++ Start Over/src/Poker.ts
@@ -1,63 +1,74 @@
 // Poker
 
 
+
+export enum ParseError {
+    InvalidFace = "Invalid Face",
+    InvalidSuit = "Invalid Suit",
+    InvalidCard = "Invalid Card"
+}
+
+
+export enum Suit {
+    Spades = "Spades",
+    Hearts = "Hearts",
+    Diamonds = "Diamonds",
+    Clubs = "Clubs"
+}
+
 // Get face value of card from input string
 
-export function GetFace(GetFaceInput: string): number | string {
+export function GetFace(GetFaceInput: string): number | ParseError.InvalidFace {
 
-    let unparsedFace: string = GetFaceInput [0];
-    let parsedFaceOutput  : number | string = 0;
-    
-    if( unparsedFace === 'T'){
+    let unparsedFace: string = GetFaceInput[0];
+    let parsedFaceOutput: number = 0;
+
+    if (unparsedFace === 'T') {
         parsedFaceOutput = 10;
     }
-    else if(
-        unparsedFace === 'J'){
+    else if (unparsedFace === 'J') {
         parsedFaceOutput = 11;
     }
-    else if(
-        unparsedFace === 'Q'){
+    else if (unparsedFace === 'Q') {
         parsedFaceOutput = 12;
     }
-    else if(
-        unparsedFace === 'K'){
+    else if (unparsedFace === 'K') {
         parsedFaceOutput = 13;
     }
-    else if(
-        unparsedFace === 'A'){
+    else if (unparsedFace === 'A') {
         parsedFaceOutput = 14;
     }
-    else{
+    else {
         parsedFaceOutput = parseInt(unparsedFace)
-    
-    if (isNaN(parsedFaceOutput) || parsedFaceOutput < 2 || parsedFaceOutput > 9) {
-        parsedFaceOutput = 'not a valid face value';
-      }
+
+        if (isNaN(parsedFaceOutput) || parsedFaceOutput < 2 || parsedFaceOutput > 9) {
+            return ParseError.InvalidFace;
+        }
     }
-return parsedFaceOutput
+    return parsedFaceOutput
 }
 
 
 // Get suit of card from input string
 
-export function GetSuit(getSuitInput: string): string{
+export function GetSuit(getSuitInput: string): Suit | ParseError.InvalidSuit {
 
-    let unparsedSuit: string = getSuitInput [1]
-    let parsedSuit:   string = 'not a valid suit'
+    let unparsedSuit: string = getSuitInput[1]
+    let parsedSuit: Suit | ParseError.InvalidSuit = ParseError.InvalidSuit
 
-    if(unparsedSuit === 'S'){
-        parsedSuit = 'Spades'
+    if (unparsedSuit === 'S') {
+        parsedSuit = Suit.Spades
     }
-    if(unparsedSuit === 'H'){
-        parsedSuit = 'Hearts'
+    if (unparsedSuit === 'H') {
+        parsedSuit = Suit.Hearts
     }
-    if(unparsedSuit === 'D'){
-        parsedSuit = 'Diamonds'
+    if (unparsedSuit === 'D') {
+        parsedSuit = Suit.Diamonds
     }
-    if(unparsedSuit === 'C'){
-        parsedSuit = 'Clubs'
+    if (unparsedSuit === 'C') {
+        parsedSuit = Suit.Clubs
     }
-return parsedSuit
+    return parsedSuit
 }
 
 
@@ -65,57 +76,176 @@ return parsedSuit
 
 // Cet the card as an object from a poker card string or return an eeror string
 
-export function GetCard(getCardInput : string): Card | string {
+export function GetCard(getCardInput: string): Card | ParseError {
+    let parsedFaceOutput = GetFace(getCardInput)
+    let parsedSuit = GetSuit(getCardInput)
 
-    let parsedFaceOutput : number | string = GetFace(getCardInput)
-    let parsedSuit : string                = GetSuit(getCardInput)
-
-    let Card1 : Card = {
-        face : parsedFaceOutput,
-        suit : parsedSuit
+    if (parsedFaceOutput === ParseError.InvalidFace && parsedSuit === ParseError.InvalidSuit) {
+        return ParseError.InvalidCard
     }
-    if(parsedFaceOutput === 'not a valid face value' && parsedSuit === 'not a valid suit'){
-    return 'not a valid face value and not a valid suit value'
-}
-    if(parsedFaceOutput === 'not a valid face value' ) {
-        return 'not a valid face value'
-     }
-    if(parsedSuit === 'not a valid suit'){
-        return 'not a valid suit'
+    if (parsedFaceOutput === ParseError.InvalidFace) {
+        return ParseError.InvalidFace
     }
-return Card1
-}
+    if (parsedSuit === ParseError.InvalidSuit) {
+        return ParseError.InvalidSuit
+    }
 
+    return {
+        face: parsedFaceOutput,
+        suit: parsedSuit
+    }
+}
 
 // Creating a card type
 
 export interface Card {
-	face: number | string
+    face: number
     suit: string
 }
 
 
 // Creating a card type
 
-type Hand = Card[]
+export type Hand = Card[]
 
 
 // Get the hand ans an array of Card objects or return an error string
 
-export function GetHand(handInput: string): Hand | string  {
+export function GetHand(handInput: string): Hand | ParseError {
 
-    const handArray  = handInput.split(" ")
-    let parsedArray: Hand | string = []
-    
-    for(const cardstring of handArray)
-    if(cardstring.length === 2){
-    const card  = GetCard(cardstring)
+    const handArray: string[] = handInput.split(" ")
+    const parsedArray: Hand = []
 
-    if(typeof card === 'object'){
-    parsedArray.push(card)
+    for (let cardstring of handArray) {
+        // console.log("Card: " + cardstring); // "2D" ... "TD" .... "5S"
+
+        if (cardstring.length === 2) {
+            const card = GetCard(cardstring)
+
+            if (typeof card === 'object') {
+                parsedArray.push(card)
+            } else {
+                return card
+            }
+        } else {
+            return ParseError.InvalidCard
+        }
     }
-    if(typeof card === 'string'){
-    return card
-    }}
-return parsedArray
+
+    return parsedArray
 }
+
+// RED GREEN REFACTOR
+export function CountFaces(hand: Hand): { [key: string]: number } {
+    let faceCounts: any = {};
+    for (let card of hand) {
+        if (faceCounts[card.face] === undefined) {
+            faceCounts[card.face] = 1;
+        } else {
+            let previousCount = faceCounts[card.face]
+            faceCounts[card.face] = previousCount + 1;
+        }
+    }
+    return faceCounts;
+}
+
+export function DetectPair(hand: Hand): boolean {
+    return DetectOfAKind(2, hand);
+}
+
+export function DetectThreeOfAKind(hand: Hand): boolean {
+    return DetectOfAKind(3, hand);
+}
+
+export function DetectFourOfAKind(hand: Hand): boolean {
+    return DetectOfAKind(4, hand);
+}
+
+export function DetectOfAKind(searchCount: number, hand: Hand): boolean {
+    let faceCounts = CountFaces(hand)
+    let counts = Object.values(faceCounts)
+
+    for (let count of counts) {
+        if (count === searchCount) {
+            return true
+        }
+    }
+    return false
+}
+
+
+
+
+
+
+
+
+
+
+
+// export enum ParseError {
+//     InvalidFace = 'Invalid Face',
+//     InvalidSuit = 'Invalid Suit',
+//     // card = 'Invalid Card'
+// }
+// export enum Suit {
+//     Spades = 'Spades',
+//     Hearts = 'Hearts',
+//     Diamonds = 'Diamonds',
+//     Clubs = 'Clubs'
+// }
+// export function GetFace(GetFaceInput: string): number | ParseError {
+//     let unparsedFace = GetFaceInput[0]
+
+//     if (unparsedFace === 'T') {
+//         return 10
+//     }
+//     if (unparsedFace === 'J') {
+//         return 11
+//     }
+//     if (unparsedFace === 'Q') {
+//         return 12
+//     }
+//     if (unparsedFace === 'K') {
+//         return 13
+//     }
+//     if (unparsedFace === 'A') {
+//         return 14
+//     }
+
+//     else {
+//         let parsedFace = parseInt(unparsedFace)
+
+//         if (isNaN(parsedFace) || parsedFace < 2 || parsedFace > 9) {
+//             return ParseError.InvalidFace;
+//         }
+//         return parsedFace
+//     }
+// }
+
+// export function GetSuit(getSuit: string): Suit | ParseError {
+//     let unparsedSuit = getSuit[1]
+
+//     if (unparsedSuit === 'S') {
+//         return Suit.Spades
+//     }
+//     else if (unparsedSuit === 'H') {
+//         return Suit.Hearts
+//     }
+//     if (unparsedSuit === 'D') {
+//         return Suit.Diamonds
+//     }
+//     if (unparsedSuit === 'C') {
+//         return Suit.Clubs
+//     }
+//     else {
+//         return ParseError.InvalidSuit
+//     }
+// }
+// export function GetCard( getCard: string): Card | ParseError {
+//     let unparsedFace = getCard[0]
+//     let unparsedSuit = getCard[1]
+
+//     let parsedFace = GetFace(unparsedFace)
+//     let par
+// }
--- Start Over/src/__tests__/Poker.test.ts
+++ Start Over/src/__tests__/Poker.test.ts
@@ -1,79 +1,105 @@
-import {Card, GetFace, GetSuit, GetCard, GetHand} from "../Poker.ts";;
+import { DetectFourOfAKind, DetectThreeOfAKind, Hand, DetectPair, Suit, Card, GetFace, GetSuit, GetCard, GetHand, ParseError } from "../Poker.ts";;
 
 
 test('Get face value a playing card', () => {
-    expect (GetFace('TH')).toBe(10)
-    expect (GetFace('JH')).toBe(11)
-    expect (GetFace('QH')).toBe(12)
-    expect (GetFace('KH')).toBe(13)
-    expect (GetFace('AH')).toBe(14)
-    expect (GetFace('2H')).toBe(2)
-    expect (GetFace('3H')).toBe(3)
-    expect (GetFace('4H')).toBe(4)
-    expect (GetFace('5H')).toBe(5)
-    expect (GetFace('6H')).toBe(6)
-    expect (GetFace('7H')).toBe(7)
-    expect (GetFace('9H')).toBe(9)
-    expect (GetFace('PH')).toBe('not a valid face value')
+    expect(GetFace('TH')).toBe(10)
+    expect(GetFace('JH')).toBe(11)
+    expect(GetFace('QH')).toBe(12)
+    expect(GetFace('KH')).toBe(13)
+    expect(GetFace('AH')).toBe(14)
+    expect(GetFace('2H')).toBe(2)
+    expect(GetFace('3H')).toBe(3)
+    expect(GetFace('4H')).toBe(4)
+    expect(GetFace('5H')).toBe(5)
+    expect(GetFace('6H')).toBe(6)
+    expect(GetFace('7H')).toBe(7)
+    expect(GetFace('9H')).toBe(9)
+    expect(GetFace('PH')).toBe(ParseError.InvalidFace)
 })
 
 
 test('get the suit from a playing card', () => {
-    expect(GetSuit('AS')).toBe('Spades')
-    expect(GetSuit('8H')).toBe('Hearts')
-    expect(GetSuit('2D')).toBe('Diamonds')
-    expect(GetSuit('TC')).toBe('Clubs')
-    expect(GetSuit('KP')).toBe('not a valid suit')
+    expect(GetSuit('AS')).toBe(Suit.Spades)
+    expect(GetSuit('8H')).toBe(Suit.Hearts)
+    expect(GetSuit('2D')).toBe(Suit.Diamonds)
+    expect(GetSuit('TC')).toBe(Suit.Clubs)
+    expect(GetSuit('KP')).toBe(ParseError.InvalidSuit)
 })
 
 test('get the card from a poker card string', () => {
 
-    let cardA : Card |string = GetCard('2H')
+    let cardA: Card | string = GetCard('2H')
     if (typeof cardA === 'object' && cardA !== null) {
-    expect (cardA.face).toBe(2)
-    expect (cardA.suit).toBe('Hearts')
+        expect(cardA.face).toBe(2)
+        expect(cardA.suit).toBe(Suit.Hearts)
     }
-    let cardB : Card |string = GetCard('7C')
+    let cardB: Card | string = GetCard('7C')
     if (typeof cardB === 'object' && cardB !== null) {
-    expect (cardB.face).toBe(7)
-    expect (cardB.suit).toBe('Clubs')
+        expect(cardB.face).toBe(7)
+        expect(cardB.suit).toBe(Suit.Clubs)
     }
-    let card1 : Card |string = GetCard('KH')
+    let card1: Card | string = GetCard('KH')
     if (typeof card1 === 'object' && card1 !== null) {
-    expect (card1.face).toBe(13)
-    expect (card1.suit).toBe('Hearts')
+        expect(card1.face).toBe(13)
+        expect(card1.suit).toBe(Suit.Hearts)
     }
-    let card2 : Card |string = GetCard('RD')
-    expect (card2).toBe('not a valid face value')
+    let card2: Card | string = GetCard('RD')
+    expect(card2).toBe(ParseError.InvalidFace)
 
-    let card3 : Card |string = GetCard('TW')
-    expect (card3).toBe('not a valid suit')
+    let card3: Card | string = GetCard('TW')
+    expect(card3).toBe(ParseError.InvalidSuit)
 
     let card4: Card | string = GetCard('WL')
-    expect (card4).toBe('not a valid face value and not a valid suit value')
-    
+    expect(card4).toBe(ParseError.InvalidCard)
+
 })
+
 test('get the card from a poker card string', () => {
     expect(GetHand('2H TS 9C AS 7D')).toEqual([{
         face: 2,
-        suit : 'Hearts'
-    },{
+        suit: 'Hearts'
+    }, {
         face: 10,
-        suit : 'Spades'
-    },{
+        suit: 'Spades'
+    }, {
         face: 9,
-        suit : 'Clubs'
-    },{
+        suit: 'Clubs'
+    }, {
         face: 14,
-        suit : 'Spades'
-    },{
+        suit: 'Spades'
+    }, {
         face: 7,
-        suit : 'Diamonds'
-    }])
+        suit: 'Diamonds'
+    }
+    ])
 
-    expect(GetHand('XH TS 9C AS 7D')).toBe('not a valid face value')
+    expect(GetHand('XH TS 9C AS 7D')).toBe(ParseError.InvalidFace)
 
-    expect(GetHand('2H TS 9C AP 7D')).toBe('not a valid suit')
- 
-    expect(GetHand('2H TS 9C LP 7D')).toBe('not a valid face value and not a valid suit value')
- })
+    expect(GetHand('2H TS 9C AP 7D')).toBe(ParseError.InvalidSuit)
+
+    expect(GetHand('2H TS 9C LP 7D')).toBe(ParseError.InvalidCard)
+    expect(GetHand('2 TS')).toBe(ParseError.InvalidCard)
+})
+
+test('detect pair', () => {
+    expect(DetectPair(GetHand('2H 8S 9C AS 7D') as Hand)).toBe(false);
+    expect(DetectPair(GetHand('2H 2S 9C AS 7D') as Hand)).toBe(true);
+    expect(DetectPair(GetHand('7H 7S 9C AS 7D') as Hand)).toBe(false);
+})
+
+test('detect three of a kind', () => {
+    expect(DetectThreeOfAKind(GetHand('2H 8S 9C AS 7D') as Hand)).toBe(false);
+    expect(DetectThreeOfAKind(GetHand('2H 8S 9C 8S 8D') as Hand)).toBe(true);
+})
+
+test('detect four of a kind', () => {
+    expect(DetectFourOfAKind(GetHand('2H 8S 9C AS 7D') as Hand)).toBe(false);
+    expect(DetectFourOfAKind(GetHand('8H 8S 9C 8S 8D') as Hand)).toBe(true);
+})
+
+
+// full house
+// two pair
+// flush
+
+// straight
